0e9917246208a3c7de59724c4265c77ae7cd299b
AMITKUMAR0712 <amittalan2203@gmail.com>
2025-09-30 13:16:20 +0530

    RamhariEnterprises.com website

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -21,27 +21,31 @@ export default function Navbar() {
       <div className="max-w-10xl mx-auto px-2 sm:px-4 lg:px-6">
         <div className="flex justify-between h-26">
           {/* Logo */}
-          <div className="flex items-center">
-            <Link href="/" onClick={() => setIsOpen(false)}>
-              <motion.div
-                initial={{ scale: 0.8, opacity: 0 }}
-                animate={{ scale: 1, opacity: 1 }}
-                transition={{ duration: 0.6, ease: "easeOut" }}
-                whileHover={{ scale: 1.1, rotate: 5 }}
-              >
-                <Image
-                  src="/images/RH_logo-removebg-preview.png"
-                  alt="Logo"
-                  width={190}
-                  height={145}
-                  priority
-                />
-              </motion.div>
-            </Link>
-          </div>
+     <div className="flex items-center">
+  <Link href="/" onClick={() => setIsOpen(false)}>
+    <motion.div
+      initial={{ scale: 0.8, opacity: 0 }}
+      animate={{ scale: 1, opacity: 1 }}
+      transition={{ duration: 0.6, ease: "easeOut" }}
+      whileHover={{ scale: 1.1, rotate: 5 }}
+      className="inline-flex items-center justify-center w-24 h-23 rounded-full bg-white overflow-hidden shadow-md"
+    >
+      <Image
+      className="w-78 h-108 object-contain"
+        src="/images/RH_logo-removebg-preview.png"
+        alt="Logo"
+        width={200}   
+        height={200}
+        priority
+      />
+    </motion.div>
+  </Link>
+</div>
+
+
 
           {/* Desktop Nav */}
-          <div className="hidden md:flex space-x-6 items-center">
+          <div className="hidden md:flex space-x-6 items-center ">
             {navItems.map((item) => (
               <motion.div
                 key={item.label}
@@ -50,10 +54,10 @@ export default function Navbar() {
               >
                 <Link
                   href={item.href}
-                  className={`px-3 py-2 rounded-md text-base font-medium transition-colors duration-300 ${
+                  className={`px-3 py-2 rounded-md text-base font-medium transition-colors duration-300  ${
                     pathname === item.href
-                      ? "text-blue-400 font-semibold"
-                      : "text-white hover:text-blue-300"
+                      ? "text-blue-400 font-extrabold"
+                      : "text-amber-500 hover:text-blue-300 font-extrabold"
                   }`}
                 >
                   {item.label}
